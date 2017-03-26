d2d792710e66a16853f957a8a418f026a3af740f
Stephan Kulla <git.mail@kulla.me>
2017-03-26 01:41:36 +0100

    Add creation of nested JSON in parsing a sitemap

--- parse_sitemap.py
+++ parse_sitemap.py
@@ -1,5 +1,6 @@
 """Module for parsing the sitemap of MFNF into a JSON file."""
 
+import json
 import re
 import requests
 
@@ -14,7 +15,7 @@ class Sitemap:
         return {"code": code, "depth": depth, "children": []}
 
     @staticmethod
-    def compute_tokens(sitemap):
+    def yield_nodes(sitemap):
         """Generator for all node specifications in a sitemap. It yields tuples
         `(code, depth)` whereas `code` is a string representation of the node
         and `depth` is a number corresponding to the depth the node corresponds
@@ -36,23 +37,36 @@ class Sitemap:
                     yield Sitemap.create_node(code, depth)
 
     @staticmethod
+    def insert_node(base, new_node):
+        """Inserts a node at the right position."""
+        if (len(base["children"]) > 0 and
+                new_node["depth"] > base["children"][-1]["depth"]):
+            Sitemap.insert_node(base["children"][-1], new_node)
+        else:
+            base["children"].append(new_node)
+
+    @staticmethod
     def parse(sitemap):
         """Parse the sitemap and returns a JSON object of it.
 
         Arguments:
             sitemap -- content of the sitemap (a string)
         """
-        for node in Sitemap.compute_tokens(sitemap):
-            print(node)
+        root = {"children":[], "depth":0}
 
+        for node in Sitemap.yield_nodes(sitemap):
+            Sitemap.insert_node(root, node)
 
-def run_script():
+        return root
+
+def run_script(json_file_name):
     """Parses the sitmap of MFNF and stores it into a JSON file."""
     session = MediaWikiSession("de.wikibooks.org", requests.Session())
     wikibooks = MediaWikiAPI(session)
     sitemap = wikibooks.get_content("Mathe für Nicht-Freaks: Sitemap")
 
-    Sitemap.parse(sitemap)
+    with open(json_file_name, "w") as json_file:
+        json.dump(Sitemap.parse(sitemap), json_file, sort_keys=True, indent=4)
 
 if __name__ == "__main__":
-    run_script()
+    run_script("sitemap.json")
