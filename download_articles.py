serlo/serlo-export

commit 1144b1788179547e0cb59558caedbb87655465d7

PEP8 file "download_article.py"

download_articles.py
@@ -4,16 +4,15 @@ structure.
 Copyright 2017 Stephan Kulla
 """
 
-import functools
-import gevent
 import json
 import os
-
-from gevent import monkey
-monkey.patch_all()
+import functools
+import gevent
+import config
 
 from api import MediaWikiAPI
-import config
+
+gevent.monkey.patch_all()
 
 def remove_special_characters(word):
     """Clear a word from special characters."""
@@ -29,9 +28,9 @@ def download(node, storage_path, wikibook_api):
         storage_path -- directory for download (string)
         wikibook_api -- MediaWikiAPI object to perform the download
     """
-    name        = remove_special_characters(node["name"])
-    link        = node["link"]
-    children    = node["children"]
+    name = remove_special_characters(node["name"])
+    link = node["link"]
+    children = node["children"]
     target_path = os.path.join(storage_path, name)
     if children:
         os.makedirs(target_path, exist_ok=True)
@@ -40,8 +39,8 @@ def download(node, storage_path, wikibook_api):
         gevent.joinall(jobs)
     if link and not children:
         contents = wikibook_api.get_content(link)
-        with open(target_path + ".txt", 'w') as f:
-            f.write(contents)
+        with open(target_path + ".txt", 'w') as output_file:
+            output_file.write(contents)
 
 def run_script(sitemap_file_name):
     """Recursively download a sitemap.

commit c241355b52e166a7f2ee2ab5f8495c3409522166

download_articles.py: fixed error introduced in 1144b1788179547e0cb59558caedbb87655465d7

download_articles.py
@@ -8,6 +8,7 @@ import json
 import os
 import functools
 import gevent
+import gevent.monkey
 
 from api import MediaWikiAPI
 from parse_sitemap import get_sitemap