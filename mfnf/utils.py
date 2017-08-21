3e20f9ffa0449df3559d425f987acecc2d25aa22
Stephan Kulla <git.mail@kulla.me>
2017-08-21 21:24:54 +0200

    utils: Add function add_dict()

--- mfnf/utils.py
+++ mfnf/utils.py
@@ -3,3 +3,11 @@
 def remove_prefix(text, prefix):
     """Removes the prefix `prefix` from string `text` in case it is present."""
     return text[len(prefix):] if text.startswith(prefix) else text
+
+def add_dict(dct1, dct2):
+    """Returns a new dictionaries where the content of the dictionaries `dct1`
+    and `dct2` are merged together."""
+    result = dct1.copy()
+    result.update(dct2)
+
+    return result
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from mfnf.utils import remove_prefix
+from mfnf.utils import remove_prefix, add_dict
 
 class TestUtilsFunctions(TestCase):
 
@@ -9,3 +9,18 @@ class TestUtilsFunctions(TestCase):
         self.assertEqual(remove_prefix("aa", "aaa"), "aa")
         self.assertEqual(remove_prefix("a 4 2", "a 4"), " 2")
         self.assertEqual(remove_prefix("", "a 4"), "")
+
+    def test_add_dict(self):
+        for dict1, dict2, output in [
+                ({"a": 1}, {"b": 2}, {"a": 1, "b": 2}),
+                ({"a": 1}, {"a": 2}, {"a": 2}),
+                ({},       {"a": 2}, {"a": 2}),
+                ({"a": 2}, {},       {"a": 2})]:
+            dict1_before = dict1.copy()
+            dict2_before = dict2.copy()
+
+            self.assertDictEqual(add_dict(dict1, dict2), output)
+
+            # dict1 and dict2 didn't change during execution of add_dict()
+            self.assertDictEqual(dict1, dict1_before)
+            self.assertDictEqual(dict2, dict2_before)
