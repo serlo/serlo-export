dbb818cd6e558ef204cbe741d087833029f60f75
Stephan Kulla <git.mail@kulla.me>
2017-08-22 15:53:21 +0200

    docs: Rewrite formatting-elements.py

--- documentation/formatting-elements.py
+++ documentation/formatting-elements.py
@@ -66,88 +66,76 @@ THEOREM = {
     "explanation": List[Dict],
     "example": List[Dict],
     "summary": List[Dict],
+    "solution_process": List[Dict]
     "proof": List[Dict],
-    "proof2": List[Dict]
+    "alternative_proof": List[Dict]
 }
 
-# An exercise
-# title:Dict        the title of the task as a TextLine
-# contents:Dict     the task itself as a TextLine
-# explanation:Dict  an explanation of the task as a TextLine (optional)
-# solution:Dict     how to get to the proof as a TextLine (optional)
-# summary:Dict      a proof summary as a TextLine (optional)
-# proof:Dict        the proof requesed in the task as a TextLine (optional)
-exercise = {"type": Format.Exercise, "title": {}, "contents": {}, "explanation":
-            {}, "solution": {}, "summary": {}, "proof": {}}
+PROOF = {
+    "type": "proof",
+    "title": str,
+    "proof": List[Dict]
+}
 
-# A hint
-# contents:Dict the hint as a TextLine
-hint = {"type": Format.Hint, "contents": {}}
+ALTERNATIVE_PROOF = {
+    "type": "alternative_proof",
+    "title": str,
+    "alternative_proof": List[Dict]
+}
 
-# A warning
-# contents:Dict the warning as a TextLine
-warning = {"type": Format.Warning, "contents": {}}
+PROOF_SUMMARY = {
+    "type": "proof_summary",
+    "title": str,
+    "proof_summary": List[Dict]
+}
 
-# A proof
-# title:Dict    the title of the proof as a TextLine
-# contents:Dict the proof as a TextLine
-proof = {"type": Format.Proof, "contents": {}}
+SOLUTION_PROCESS = {
+    "type": "solution_process",
+    "title": str,
+    "solution_process": List[Dict]
+}
 
-# An alternative proof
-# Must this be a separate template?
-# title:str     the title of the proof
-# contents:Dict the proof as a TextLine
-alternative_proof = {"type": Format.AlternativeProof, "title": {},
-                     "contents": {}}
+SOLUTION = {
+    "type": "solution",
+    "title": str,
+    "solution": List[Dict]
+}
 
-# A proof summary
-# title:str     the title of the proof
-# contents:Dict the proof as a TextLine
-proof_summary = {"type": Format.ProofSummary, "title": {},
-                 "contents": {}}
+EXERCISE = {
+    "type": "exercise",
+    "title": str,
+    "explanation": List[Dict],
+    "exercise": List[Dict],
+    "solution": List[Dict],
+    "summary": List[Dict],
+    "solution_process": List[Dict]
+    "proof": List[Dict],
+    "alternative_proof": List[Dict]
+}
 
-# A solution to an exercise
-# title:str     the title of the solution
-# contents:Dict the solution as a TextLine
-solution = {"type": Format.Solution, "title": {},
-            "contents": {}}
+HINT = {
+    "type": "hint",
+    "hint": List[Dict]
+}
 
-# A detailed explanation of a solution
-# title:str     the title of the solution explanation
-# contents:Dict the solution explanation as a TextLine
-solution_explanation = {"type": Format.SolutionExplanation, "title": {},
-                        "contents": {}}
+WARNING = {
+    "type": "warning",
+    "warning": List[Dict]
+}
 
-# A question
-# type:str      the question type ("Verständnisfrage") (optional)
-# answer:Dict   the answer as a TextLine
-# indent:bool   whether list answers should be indented
-question = {"type": Format.Question, "type": "", "answer": {}, "indent": True}
+QUESTION = {
+    "type": "question",
+    "question": List[Dict],
+    "answer": List[Dict]
+}
 
-# A step in a proof
-# name:str      the name of the proof step
-# target:Dict   what is proved in this step as a TextLine
-# contents:Dict the proof step as a TextLine
-proof_step = {"type": Format.ProofStep, "name": "", "target": {}, "contents": {}}
+PROOF_STEP = {
+    "type": "proof_step",
+    "target": List[Dict],
+    "name": str,
+    "children": List[Dict]
+}
 
-# A case discrimination (e.g. in a proof)
-# contents:List[Dict]   the individual cases, each as Dict of
-#   case:Dict   what the case is as a TextLine
-#   proof:Dict  proof of the case as a TextLine
-case_discrimination = {"type": Format.CaseDiscrimination, "contents": []}
+CASE_DISCRIMINATION = {...}
 
-# Inline math code (Tex)
-# contents:str  the Tex code
-math_inline = {"type": Format.MathInline, "contents": ""}
-
-# A piece of math code in its own paragraph
-# contents:str  the Tex code
-formula = {"type": Format.Formula, "contents": ""}
-
-# A reference to an anchor (e.g. next to a heading) or an exercise
-# page: str     the page to which is linked
-# anchor:str    the referenced anchor at the page
-# contents:str  the text displayed in place of the hyperlink
-#               for the print version, we might want to skip this and display a
-#               number, e.g. "Exercise 1.2.3"
-reference = {"type": Format.Reference, "page": "", "anchor": "", "contents": ""}
+REFERENCE = {...}
