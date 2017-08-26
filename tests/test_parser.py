9c61b0d27873c8c1ea2ba2311f547625a83bf7be
Stephan Kulla <git.mail@kulla.me>
2017-08-26 22:47:05 +0200

    test: Add cached_function to API

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,14 +1,37 @@
+import shelve
+
+from unittest import TestCase
+
 import requests
 import yaml
 
-from unittest import TestCase
 from mfnf.api import HTTPMediaWikiAPI
 from mfnf.parser import HTML2JSONParser, ArticleContentParser
+from mfnf.utils import CachedFunction
 
 class TestParser(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.database = shelve.open(".cache.db", "c")
+        cached_function = CachedFunction(cls.database)
+
+        class CachedMediaWikiAPI(HTTPMediaWikiAPI):
+            @cached_function
+            def get_content(self, title):
+                return super().get_content(title)
+
+            @cached_function
+            def convert_text_to_html(self, title, text):
+                return super().convert_text_to_html(title, text)
+
+        cls.api = CachedMediaWikiAPI(requests.Session())
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.database.close()
+
     def setUp(self):
-        self.api = HTTPMediaWikiAPI(requests.Session())
         self.title = "Mathe für Nicht-Freaks: Analysis 1"
         self.maxDiff = None
 
