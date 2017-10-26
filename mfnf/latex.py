91bfb5343c253cd1a9eb3521d9f7da48bd953c43
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2017-10-26 20:42:18 +0200

    Revert "latex: Delete \maketitle"
    This reverts commit 116baefce59ceb38b760ffcb6d7b9a377c9a9b58.

--- mfnf/latex.py
+++ mfnf/latex.py
@@ -358,6 +358,7 @@ class LatexExporter:
         out.write("\\sloppy\n\n")
         out.write("\\includepdf[pages=-,addtotoc={4,part,-1,Über das Buchprojekt,sec:about}]{../../predesigned_pages/mfnf_prelude}\n\n")
 
+        out.write("\\maketitle\n\n")
         out.write("\\ColoredTOC\n\n")
         out.write("\\newpage\n\n")
 
