116baefce59ceb38b760ffcb6d7b9a377c9a9b58
Stephan Kulla <git.mail@kulla.me>
2017-10-26 19:02:08 +0200

    latex: Delete \maketitle

--- mfnf/latex.py
+++ mfnf/latex.py
@@ -358,7 +358,6 @@ class LatexExporter:
         out.write("\\sloppy\n\n")
         out.write("\\includepdf[pages=-,addtotoc={4,part,-1,Über das Buchprojekt,sec:about}]{../../predesigned_pages/mfnf_prelude}\n\n")
 
-        out.write("\\maketitle\n\n")
         out.write("\\ColoredTOC\n\n")
         out.write("\\newpage\n\n")
 
