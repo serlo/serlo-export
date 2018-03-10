86688f53a5d98b45d26e62e621559d4017f0c805
Stephan Kulla <git.mail@kulla.me>
2018-03-10 18:26:12 +0100

    make: Fix typos "ARCTICLE" -> "ARTICLE"

--- mk/article.mk
+++ mk/article.mk
@@ -1,10 +1,10 @@
 REVISIONS = revisions
 
 $(REVISIONS):
-	$(eval ARCTICLE := $(patsubst %/,%,$(dir $(MAKECMDGOALS))))
+	$(eval ARTICLE := $(patsubst %/,%,$(dir $(MAKECMDGOALS))))
 	$(eval REVISION := $(notdir $(MAKECMDGOALS)))
-	@[ -d $(ARCTICLE) ] || mkdir $(ARCTICLE)
-	$(MAKE) -C $(ARCTICLE) -f $(MK)/revision.mk ARTICLE=$(ARCTICLE) MK=$(MK) $(REVISION)
+	@[ -d $(ARTICLE) ] || mkdir $(ARTICLE)
+	$(MAKE) -C $(ARTICLE) -f $(MK)/revision.mk ARTICLE=$(ARTICLE) MK=$(MK) $(REVISION)
 
 % :: $(REVISIONS) ;
 
