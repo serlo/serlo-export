a46635d293c464b5762d91c19cb1cffe2a3490f1
Valentin Roland <valentin@vroland.de>
2018-05-16 00:04:54 +0200

    inline section generation for easier comprehension / debugability

--- mk/section.mk
+++ mk/section.mk
@@ -3,7 +3,12 @@ BUILD_YML = build_yml
 REVID := $(shell $(MK)/resolve_revid.sh $(ARTICLE) $(MAKECMDGOALS))
 
 $(THE_SECTIONS): $(BASE)/articles/$(ARTICLE)/$(REVID).yml
-	$(MK)/article_sections.sh $(ARTICLE) < $<
+	$(MK)/bin/mfnf_ex --config $(BASE)/config/mfnf.yml \
+		--title $(ARTICLE) \
+		--section-path $(BASE)/sections \
+		--externals-path $(BASE)/media \
+		--texvccheck-path $(MK)/bin/texvccheck \
+		sections $(ARTICLE) < $<
 
 $(BASE)/articles/%.yml :: $(BUILD_YML) ;
 
