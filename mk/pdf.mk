0ae084503a75ccb520da58100f14762347677edf
Valentin Roland <valentin@vroland.de>
2018-05-15 23:52:55 +0200

    inline template engine invocation for easier understanding

--- mk/pdf.mk
+++ mk/pdf.mk
@@ -12,16 +12,25 @@ LATEX := lualatex
 	TEXINPUTS=$(BASE): latexmk -quiet -pdflatex="$(LATEX) %O %S" -pdf $<
 
 %.tex: %.yml $(LATEX_ARTICLE)/%.tex
-	$(MK)/article_tex_template.sh $(CURDIR)/$< $(word 2,$^) $(ARTICLE) $* > $@
+	$(MK)/bin/handlebars-cli-rs \
+		--input $(BASE)/templates/article.tex \
+		--data $< \
+		content $(word 2,$^) \
+		article $(ARTICLE) \
+		revision $(REVISION) \
+		fontpath $(BASE)/karmilla/ttf/ \
+		> $@
 
+# generate article template values
 %.yml: $(ORIGIN)/%.yml
-	$(MK)/article_to_pdf.sh $(ARTICLE) $* $(TARGET).$(SUBTARGET) < $< > $@
+	$(MK)/bin/mfnf_ex -c $(BASE)/config/mfnf.yml \
+		--title $(ARTICLE) \
+		--revision $(REVISION) \
+		$(TARGET).$(SUBTARGET) < $< > $@
 
 $(LATEX_ARTICLE)/% :: $(RECURSE_TO_LATEX_ARTICLE) ;
 
 $(ORIGIN)/% :: $(RECURSE_TO_ORIGIN) ;
-
-.PHONY: $(RECURSE_TO_LATEX_ARTICLE) $(RECURSE_TO_ORIGIN)
 
 $(RECURSE_TO_LATEX_ARTICLE):
 	$(MAKE) -C $(BASE) article_exports/latex/$(SUBTARGET)/$(ARTICLE)/$(REVISION).tex
@@ -29,6 +38,6 @@ $(RECURSE_TO_LATEX_ARTICLE):
 $(RECURSE_TO_ORIGIN):
 	$(MAKE) -C $(BASE) articles/$(ARTICLE)/$(REVISION).yml
 
+.PHONY: $(RECURSE_TO_LATEX_ARTICLE) $(RECURSE_TO_ORIGIN)
 .DELETE_ON_ERROR:
-
 .SECONDARY:
