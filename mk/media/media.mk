87754d06f0aefa0ce6033e818b50b4063c705cda
Valentin Roland <valentin@vroland.de>
2018-05-25 17:45:52 +0200

    allow uppercase media extensions

--- mk/media/media.mk
+++ mk/media/media.mk
@@ -1,28 +1,45 @@
-%.gif.qr.svg %.webm.qr.svg %.mp4.qr.svg:
+INKSCAPE = inkscape --without-gui --export-area-page --export-text-to-path \
+	--export-ignore-filters --export-pdf=$@ $<
+CONVERT = convert $< $@
+DOWNLOAD_PDF = python $(MK)/download_image.py $*.pdf > $@
+
+%.gif.qr.svg %.GIF.qr.svg %.webm.qr.svg %.WEBM.qr.svg %.mp4.qr.svg %.MP4.qr.svg:
 	qrencode -o - -t SVG "https://commons.wikimedia.org/wiki/File:`echo $@ | sed 's/.qr.*//g'`" > $@
 
 %.jpg.pdf: %.jpg
-	convert $< $@
+	$(CONVERT)
+
+%.JPG.pdf: %.JPG
+	$(CONVERT)
 
 %.png.pdf: %.png
-	convert $< $@
+	$(CONVERT)
+
+%.PNG.pdf: %.PNG
+	$(CONVERT)
 
 %.svg.pdf: %.svg
-	inkscape --without-gui --export-area-page --export-text-to-path \
-		--export-ignore-filters --export-pdf=$@ $<
+	$(INKSCAPE)
+
+%.SVG.pdf: %.SVG
+	$(INKSCAPE)
 
 %.eps.pdf: %.eps
-	inkscape --without-gui --export-area-page --export-text-to-path \
-		--export-ignore-filters --export-pdf=$@ $<
+	$(INKSCAPE)
+
+%.EPS.pdf: %.EPS
+	$(INKSCAPE)
 
 %.qr.pdf: %.qr.svg
-	inkscape --without-gui --export-area-page --export-text-to-path \
-		--export-ignore-filters --export-pdf=$@ $<
+	$(INKSCAPE)
 
 %.plain.pdf:
-	python $(MK)/download_image.py $*.pdf > $@
+	$(DOWNLOAD_PDF)
 
-%.svg %.png %.jpg %.gif:
+%.plain.PDF:
+	$(DOWNLOAD_PDF)
+
+%.svg %.SVG %.png %.PNG %.jpg %.JPG %.jpeg %.JPEG %.gif %.GIF:
 	python $(MK)/download_image.py $@ > $@
 
 .DELETE_ON_ERROR:
