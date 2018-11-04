a955d6548ec238bb4413975f1c42ffa1eb5093ee
Valentin Roland <valentin@vroland.de>
2018-11-04 19:40:00 +0100

    make get_revision.sh dash compatible

--- mk/get_revision.sh
+++ mk/get_revision.sh
@@ -17,17 +17,17 @@ HAS_KEY=$(flock $REV_FILE jq ".$SECTION | has(\"$NAME\")" $REV_FILE);
 
 if [ $HAS_KEY != true ]; then 
     URL_ENC=$(echo $NAME | jq -r -R @uri)
-    if [ $SECTION == articles ]; then
+    REVISION=error
+    if [ $SECTION = articles ]; then
         REVISION=$(curl -qgsf "https://de.wikibooks.org/api/rest_v1/page/title/$URL_ENC" | jq ".items[0].rev")
         REVISION=$(echo "$REVISION" | grep -e "^[0-9][0-9]*$" || echo "error")
     fi
-    if [ $SECTION == media ]; then 
+    if [ $SECTION = media ]; then
         SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
         REVISION=$(python3 $SCRIPTPATH/get_image_revision.py $NAME)
         # revision has to start with year
         REVISION=$(echo "$REVISION" | grep -e "^[0-9][0-9]*" || echo "error")
     fi
-    
 
     [ $REVISION != error ] || (>&2 echo "revision fetching failed for $NAME"! && exit 1);
     # use sponge here to keep the file at the same inode, 
