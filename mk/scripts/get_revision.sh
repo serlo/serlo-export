86fa4a598a7c826247d427ed03aadf1d86e45bc4
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2019-01-04 00:04:54 +0100

    Replace image download and revision retrieving by curl

--- mk/scripts/get_revision.sh
+++ mk/scripts/get_revision.sh
@@ -3,7 +3,7 @@
 # get the revision of the article from a revision lock file.
 # if it is not currently known, it the latest revision will be fetched 
 # and stored in the lock file.
-# Arguments: <revision_file> <article name>
+# Arguments: <revision_file> <section> <article name>
 set -e
 
 NAME=$(echo "$3" | sed -e "s/ /_/g")
@@ -16,15 +16,22 @@ SECTION=$2
 HAS_KEY=$(flock -s $REV_FILE jq ".$SECTION | has(\"$NAME\")" $REV_FILE);
 
 if [ $HAS_KEY != true ]; then 
-    URL_ENC=$(echo $NAME | jq -r -R @uri)
     REVISION=error
     if [ $SECTION = articles ]; then
+        URL_ENC=$(echo $NAME | jq -r -R @uri)
         REVISION=$(curl -qgsf "https://de.wikibooks.org/api/rest_v1/page/title/$URL_ENC" | jq ".items[0].rev")
         REVISION=$(echo "$REVISION" | grep -e "^[0-9][0-9]*$" || echo "error")
     fi
     if [ $SECTION = media ]; then
-        SCRIPTPATH=$(dirname "$0")
-        REVISION=$(python3 $SCRIPTPATH/get_image_revision.py $NAME)
+        REVISION=$(curl -Gqgsf "https://de.wikibooks.org/w/api.php" \
+                        --data-urlencode "titles=File:$NAME" \
+                        --data-urlencode "prop=imageinfo" \
+                        --data-urlencode "iilimit=max" \
+                        --data-urlencode "iiprop=url|sha1|timestamp" \
+                        --data-urlencode "iistart=now" \
+                        --data-urlencode "format=json" \
+                        --data-urlencode "action=query" \
+                       | jq -r '.query.pages."-1".imageinfo | map(.timestamp) | max')
         # revision has to start with year
         REVISION=$(echo "$REVISION" | grep -e "^[0-9][0-9]*" || echo "error")
     fi
